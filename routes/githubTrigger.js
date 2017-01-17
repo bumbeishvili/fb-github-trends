9d3ef6ff2f7cb200fd8439afaca7e8828c73725e
Dato <davit.bumbeishvili@gmail.com>
2017-01-17 12:48:01 +0400

    add dynamuc promise chaining based on langs array

--- routes/githubTrigger.js
+++ routes/githubTrigger.js
@@ -1,27 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
+
 var Trending = require("github-trend");
 var scraper = new Trending.Scraper();
 
+var data = [];
+var langs = ["", "csharp", "javascript"]; // Empty string means 'all languages'
+
+
 
 router.get('/', function (req, res, next) {
-
-
-    // Empty string means 'all languages'
-    scraper.scrapeTrendingReposFullInfo("csharp").then(function (repos) {
-        repos.forEach(function (repo) {
-            console.log(repo.owner);
-            console.log(repo.name);
+    fetchTrendingRepos(langs)
+        .then(function () {
+            res.setHeader('Content-Type', 'application/json');
+            res.send(JSON.stringify(data))
+        })
+        .catch(function (err) {
+            console.log(err.message);
         });
-        res.setHeader('Content-Type', 'application/json');
-        res.send({ repos: repos });
-    }).catch(function (err) {
-        console.log(err.message);
-    });
-
 });
 
 
+
+function fetchTrendingRepos(languageArray) {
+    return languageArray.reduce((promise, language) => {
+        return promise.then(() => {
+            return scraper.scrapeTrendingReposFullInfo(language)
+                .then(repos => {
+                    data = data.concat(repos);
+                });
+        });
+    }, Promise.resolve());
+}
 
 module.exports = router;
