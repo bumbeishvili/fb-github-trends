b29632ac07970a3339a372840f8ac1e41d660ad9
Dato <davit.bumbeishvili@gmail.com>
2017-01-17 13:51:06 +0400

    added trending type,scrape time and post time props

--- routes/githubTrigger.js
+++ routes/githubTrigger.js
@@ -5,30 +5,66 @@ var router = express.Router();
 var Trending = require("github-trend");
 var scraper = new Trending.Scraper();
 
-var data = [];
-var langs = ["", "csharp", "javascript"]; // Empty string means 'all languages'
+const configs = [
+    {
+        lang: ""  // Empty string means 'all languages'
+    },
+    {
+        lang: "csharp"
+    },
+    {
+        lang: "javascript"
+    }
+];
+
+const trendingTypes = {
+    DAILY: 'DAILY',
+    WEEKLY: 'WEEKLY',
+    MONTHLY: 'MONTHLY'
+}
 
 
 
 router.get('/', function (req, res, next) {
-    fetchTrendingRepos(langs)
-        .then(function () {
-            res.setHeader('Content-Type', 'application/json');
-            res.send(JSON.stringify(data))
-        })
+    var data = [];
+    var langs = configs.map(c => c.lang);
+
+    fetchTrendingRepos(langs, data)
+        .then(processRepos.bind(this, res, data))
         .catch(function (err) {
             console.log(err.message);
         });
+
 });
 
 
+function processRepos(res, data) {
+    data.forEach(trendingRepo => {
+        trendingRepo.scrapeTime = new Date();
+        trendingRepo.postTime = new Date();
+    });
 
-function fetchTrendingRepos(languageArray) {
+
+
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(data))
+
+}
+
+
+
+
+function fetchTrendingRepos(languageArray, data) {
     return languageArray.reduce((promise, language) => {
         return promise.then(() => {
             return scraper.scrapeTrendingReposFullInfo(language)
                 .then(repos => {
-                    data = data.concat(repos);
+                    var extended = repos.map(repo => {
+                        repo.langCode = language;
+                        repo.type = trendingTypes.DAILY;
+                        return repo;
+                    });
+                    data.push.apply(data, extended);
                 });
         });
     }, Promise.resolve());
